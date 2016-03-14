e896f2daca71da3691093728930c9b3887de284b
Антон <antler@inbox.ru>
2016-03-14 12:00:35 +0700

    закоментил view TODO

--- application/views/TODO_view.php
+++ application/views/TODO_view.php
@@ -10,7 +10,8 @@ if (isset($data)) {
 } else {
     echo "<div>TODO</div>";
 }
-require_once CORE_PATH . 'gibberish-aes.php';
+
+/*require_once CORE_PATH . 'gibberish-aes.php';
 $key = hash('sha256', 'AntLer:205317');
 $data = 'Hello world';
 $iv = hash('md5', '205317:AntLer');
@@ -21,4 +22,4 @@ echo ' AES PHP:<div id="crypt">' . openssl_encrypt($data,'AES-256-CBC',hex2bin($
 echo exec('openssl enc -aes-256-cbc -in infile -out outfile -pass pass:"Secret Passphrase" -e -base64');
 
 $key = '59b6ab46d379b89d794c87b74a511fbd59b6ab46d379b89d794c87b74a511fbd';
-$iv = '0aaff094b6dc29742cc98a4bac8bc8f9';
+$iv = '0aaff094b6dc29742cc98a4bac8bc8f9';*/
